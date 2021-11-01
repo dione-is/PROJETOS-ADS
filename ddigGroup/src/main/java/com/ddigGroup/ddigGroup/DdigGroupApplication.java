a6a785df43dbe7c26955bf683b3929c829122d57
Dione dos Santos <73508786+dione-is@users.noreply.github.com>
2021-11-01 19:47:25 -0300

    adicionando projeto mavem ddig group AEP 4 semestre

---
+++ ddigGroup/src/main/java/com/ddigGroup/ddigGroup/DdigGroupApplication.java
@@ -0,0 +1,13 @@
+package com.ddigGroup.ddigGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdigGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdigGroupApplication.class, args);
+	}
+
+}
---
+++ ddigGroup/src/test/java/com/ddigGroup/ddigGroup/DdigGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddigGroup.ddigGroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdigGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
