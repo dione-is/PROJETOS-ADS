fd9ed83b8d0c13e43f097b74204dbe4d987413d2
Dione dos Santos <73508786+dione-is@users.noreply.github.com>
2021-11-18 19:31:11 -0300

    excluindo atividade de aula que estava dentro do projeto

--- ddigGroup/src/main/java/com/ddigGroup/ddigGroup/aula4s2021/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ddigGroup.ddigGroup.aula4s2021;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno, String> {
-
-}
